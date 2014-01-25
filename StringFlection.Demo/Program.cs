2db6c7e6da88a659952b51ec77b1737250c75e12
smack0007 <zachary.snow@gmail.com>
2014-01-25 15:51:28 +0100

    Added ToPropertyString() extension method and demo project.

---
+++ StringFlection.Demo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluxx.StringFlection;
+
+namespace Confluxx.StringFlection.Demo
+{
+    class Program
+    {
+        private static Person GetSampleObject()
+        {
+            var result = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 42,
+                Pets = new[] { "Dog", "Cat", "Horse" }
+            };
+
+            result.Children.Add(
+                new Person
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Age = 3
+                });
+
+            result.Children.Add(
+                new Person
+                {
+                    FirstName = "Alice",
+                    LastName = "Doe",
+                    Age = 6
+                });
+
+            result.Children.Add(
+                new Person
+                {
+                    FirstName = "Bob",
+                    LastName = "Doe",
+                    Age = 8
+                });
+
+            result.PhoneNumbers["Home"] = "555-007";
+            result.PhoneNumbers["Office"] = "555-23";
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            Person person = GetSampleObject();
+
+            Console.WriteLine(person.ToPropertyString("{FirstName} {LastName} has {Children.Count} children."));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringFlection/ObjectExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Confluxx.StringFlection
+{
+    public static class ObjectExtensions
+    {
+        public static string ToPropertyString(this object obj, string format)
+        {
+            return ToPropertyString(obj, format, null);
+        }
+
+        public static string ToPropertyString(this object obj, string format, IFormatProvider formatProvider)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            Regex reg = new Regex(@"({)([^}]+)(})", RegexOptions.IgnoreCase);
+            MatchCollection mc = reg.Matches(format);
+
+            int startIndex = 0;
+            foreach (Match m in mc)
+            {
+                Group group = m.Groups[2];
+                
+                // Append everything before the "{"
+                int length = group.Index - startIndex - 1;
+                sb.Append(format.Substring(startIndex, length));
+
+                string propertyChain = String.Empty;
+                string formatInfo = String.Empty;
+                int formatIndex = group.Value.IndexOf(":");
+                if (formatIndex == -1) 
+                {
+                    propertyChain = group.Value;
+                }
+                else 
+                {
+                    propertyChain = group.Value.Substring(0, formatIndex);
+                    formatInfo = group.Value.Substring(formatIndex + 1);
+                }
+
+                object propertyChainValue = ObjectStringFlector.GetValue(obj, propertyChain);
+                
+                // TODO: Use formatInfo if available.
+                // TODO: What happens if an exception is thrown.
+                
+                sb.Append(propertyChainValue.ToString());
+                                
+                startIndex = group.Index + group.Length + 1;
+            }
+
+            // Include the rest of the string.
+            if (startIndex < format.Length)
+                sb.Append(format.Substring(startIndex));            
+
+            return sb.ToString();
+        }
+    }
+}
